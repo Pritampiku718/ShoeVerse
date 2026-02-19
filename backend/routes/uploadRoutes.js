35c9fc53117c25bef267953224b86902213121ac
Pritam Biswas <pritampiku718@gmail.com>
2026-02-19 21:05:22 +0530

    config update for Cloudinary and fix Content-Type header for file uploads

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 /* =====================================================
    ✅ CLOUDINARY CONFIGURATION
 ===================================================== */
-cloudinary.v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
